b15fdbfd1d473bb0bf02466c68e58ca6c17393ef
Bruno Leal <bruno@flyadam.com.br>
2021-06-25 02:48:59 -0300

    segunda aula, lading

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import { Button } from './components/button'
+import { createContext, useState, useEffect } from 'react';
+
+import { Home } from './pages/Home'
+import { NewRoom } from './pages/NewRoom'
+import { BrowserRouter, Route } from 'react-router-dom';
+import { auth, firebase } from './services/firebase';
+
+import {AuthContextProvider} from './contexts/AuthContext';
+
+
 function App() {
+
+  
   return (
-    <Button></Button>
+    <AuthContextProvider>
+    <BrowserRouter>
+      <Route path="/" exact component={Home}/>
+      <Route path="/rooms/new" component={NewRoom}/>
+    </BrowserRouter>
+    </AuthContextProvider>
   );
 }
 
